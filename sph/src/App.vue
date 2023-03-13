191d0b85926ba0f030e793ed1c36b9a8531e47aa
陈俊沪 <junhuchen@126.com>
2023-03-13 21:13:30 +0800

    04

--- sph/src/App.vue
+++ sph/src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
     <Header></Header>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Header from './components/Header';
-
+import Footer from './components/Footer';
 export default {
   name: 'App',
   components: {
     Header,
+    Footer,
   },
 };
 </script>
