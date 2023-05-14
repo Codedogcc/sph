e8f0a5e154fce341df3b688f3ea3c34b7ecb1665
陈俊沪 <junhuchen@126.com>
2023-05-15 01:51:07 +0800

    073(引入购物车静态组件)

--- sph/src/router/routes.js
+++ sph/src/router/routes.js
@@ -6,8 +6,17 @@ import Login from '@/pages/Login';
 import Register from '@/pages/Register';
 import Detail from '@/pages/Detail';
 import AddCartSuccess from '@/pages/AddCartSuccess';
+import ShopCart from '@/pages/ShopCart';
 
 export default [
+  {
+    path: '/shopcart',
+    component: ShopCart,
+    //meta路由元信息,新学习的一个配置项!!!!给当前路由添加一些额外数据
+    //它的右侧是一个对象[可以有多个键值对]
+    //路由配置项：书写的时候不要胡写、乱写、瞎写【在VC组件身上获取不到,没有任何意义】
+    meta: { show: true },
+  },
   {
     path: '/addcartsuccess',
     component: AddCartSuccess,
