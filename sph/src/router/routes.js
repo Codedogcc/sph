1da44138f705567bd1b8b79c9e81ca3b723d749c
陈俊沪 <junhuchen@126.com>
2023-06-08 01:39:07 +0800

    094（添加交易trade组件并路由注册）

--- sph/src/router/routes.js
+++ sph/src/router/routes.js
@@ -7,12 +7,18 @@ import Register from '@/pages/Register';
 import Detail from '@/pages/Detail';
 import AddCartSuccess from '@/pages/AddCartSuccess';
 import ShopCart from '@/pages/ShopCart';
+import Trade from '@/pages/Trade';
 
 export default [
   {
+    path: '/trade',
+    component: Trade,
+    meta: { show: true },
+  },
+  {
     path: '/shopcart',
     component: ShopCart,
-    //meta路由元信息,新学习的一个配置项!!!!给当前路由添加一些额外数据
+    //meta路由元信息,新学习的一个配置项
     //它的右侧是一个对象[可以有多个键值对]
     //路由配置项：书写的时候不要胡写、乱写、瞎写【在VC组件身上获取不到,没有任何意义】
     meta: { show: true },
