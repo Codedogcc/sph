61b61fdd61c56087c22e92dd9044fa11593fd561
陈俊沪 <junhuchen@126.com>
2023-06-08 16:51:30 +0800

    075(购物车数据动态展示)

--- sph/src/pages/ShopCart/index.vue
+++ sph/src/pages/ShopCart/index.vue
@@ -11,101 +11,40 @@
         <div class="cart-th6">操作</div>
       </div>
       <div class="cart-body">
-        <ul class="cart-list">
+        <ul
+          class="cart-list"
+          v-for="(item, index) in cartInfoList"
+          :key="item.id"
+        >
           <li class="cart-list-con1">
-            <input type="checkbox" name="chk_list" />
+            <input
+              type="checkbox"
+              name="chk_list"
+              :checked="item.isChecked == 1"
+            />
           </li>
           <li class="cart-list-con2">
-            <img src="./images/goods1.png" />
+            <img :src="item.imgUrl" />
             <div class="item-msg">
-              米家（MIJIA） 小米小白智能摄像机增强版
-              1080p高清360度全景拍摄AI增强
+              {{ item.skuName }}
             </div>
           </li>
           <li class="cart-list-con4">
-            <span class="price">399.00</span>
+            <span class="price">{{ item.skuPrice }}</span>
           </li>
           <li class="cart-list-con5">
             <a href="javascript:void(0)" class="mins">-</a>
             <input
               autocomplete="off"
               type="text"
-              value="1"
+              :value="item.skuNum"
               minnum="1"
               class="itxt"
             />
             <a href="javascript:void(0)" class="plus">+</a>
           </li>
           <li class="cart-list-con6">
-            <span class="sum">399</span>
-          </li>
-          <li class="cart-list-con7">
-            <a href="#none" class="sindelet">删除</a>
-            <br />
-            <a href="#none">移到收藏</a>
-          </li>
-        </ul>
-
-        <ul class="cart-list">
-          <li class="cart-list-con1">
-            <input type="checkbox" name="chk_list" id="" value="" />
-          </li>
-          <li class="cart-list-con2">
-            <img src="./images/goods2.png" />
-            <div class="item-msg">
-              华为（MIJIA） 华为metaPRO 30 浴霸4摄像 超清晰
-            </div>
-          </li>
-          <li class="cart-list-con4">
-            <span class="price">5622.00</span>
-          </li>
-          <li class="cart-list-con5">
-            <a href="javascript:void(0)" class="mins">-</a>
-            <input
-              autocomplete="off"
-              type="text"
-              value="1"
-              minnum="1"
-              class="itxt"
-            />
-            <a href="javascript:void(0)" class="plus">+</a>
-          </li>
-          <li class="cart-list-con6">
-            <span class="sum">5622</span>
-          </li>
-          <li class="cart-list-con7">
-            <a href="#none" class="sindelet">删除</a>
-            <br />
-            <a href="#none">移到收藏</a>
-          </li>
-        </ul>
-
-        <ul class="cart-list">
-          <li class="cart-list-con1">
-            <input type="checkbox" name="chk_list" id="" value="" />
-          </li>
-          <li class="cart-list-con2">
-            <img src="./images/goods3.png" />
-            <div class="item-msg">
-              iphone 11 max PRO 苹果四摄 超清晰 超费电 超及好用
-            </div>
-          </li>
-          <li class="cart-list-con4">
-            <span class="price">11399.00</span>
-          </li>
-          <li class="cart-list-con5">
-            <a href="javascript:void(0)" class="mins">-</a>
-            <input
-              autocomplete="off"
-              type="text"
-              value="1"
-              minnum="1"
-              class="itxt"
-            />
-            <a href="javascript:void(0)" class="plus">+</a>
-          </li>
-          <li class="cart-list-con6">
-            <span class="sum">11399</span>
+            <span class="sum">{{ item.skuNum * item.skuPrice }}</span>
           </li>
           <li class="cart-list-con7">
             <a href="#none" class="sindelet">删除</a>
@@ -117,7 +56,7 @@
     </div>
     <div class="cart-tool">
       <div class="select-all">
-        <input class="chooseAll" type="checkbox" />
+        <input class="chooseAll" type="checkbox" :checke="isAllCheck" />
         <span>全选</span>
       </div>
       <div class="option">
@@ -126,10 +65,10 @@
         <a href="#none">清除下柜商品</a>
       </div>
       <div class="money-box">
-        <div class="chosed">已选择 <span>0</span>件商品</div>
+        <div class="chosed">已选择 <span>{{}}</span>件商品</div>
         <div class="sumprice">
           <em>总价（不含运费） ：</em>
-          <i class="summoney">0</i>
+          <i class="summoney">{{ totalPrice }}</i>
         </div>
         <div class="sumbtn">
           <router-link class="sum-btn" to="/trade">结算</router-link>
@@ -140,6 +79,8 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
   name: 'ShopCart',
   methods: {
@@ -150,6 +91,26 @@ export default {
   },
   mounted() {
     this.getData();
+  },
+  computed: {
+    ...mapGetters('shopcart', ['cartList']),
+    //购物车数据
+    cartInfoList() {
+      console.log(this.cartList, 'this.cartList');
+      return this.cartList.cartInfoList || [];
+    },
+    //计算购买产品的总价
+    totalPrice() {
+      let sum = 0;
+      this.cartInfoList.forEach((item) => {
+        sum += item.skuNum * item.skuPrice;
+      });
+      return sum;
+    },
+    //判断底部复选框是否勾选[全部产品都选中]
+    isAllCheck() {
+      return this.cartInfoList.every((item) => item.isChecked == -1);
+    }
   }
 };
 </script>
--- sph/src/store/shopcart/index.js
+++ sph/src/store/shopcart/index.js
@@ -1,19 +1,24 @@
 import { reqCartList } from '@/api';
 const state = {
-  
+  cartList: []
 };
 
 const mutations = {
-
+  GETCARTLIST(state, cartList) { state.cartList = cartList; }
 };
 const actions = {
   // 用户注册
   async getCartList({ commit }) {
     let result = await reqCartList()
     console.log("result---shopcart的获取购物车信息store", result);
+    if (result.code == 200) { commit("GETCARTLIST", result.data); }
   },
 };
 const getters = {
+  cartList(state) {
+    console.log(state, "steatesa");
+    return state.cartList[0] || {}
+  },
 };
 
 export default ({
