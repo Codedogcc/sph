cb688291f606dcdce65d2fd470cdf67600ce26bb
陈俊沪 <junhuchen@126.com>
2023-06-14 15:50:43 +0800

    103(增加个人中心静态组件)

--- sph/src/router/routes.js
+++ sph/src/router/routes.js
@@ -10,7 +10,13 @@ import ShopCart from '@/pages/ShopCart';
 import Trade from '@/pages/Trade';
 import Pay from '@/pages/Pay';
 import PaySuccess from '@/pages/PaySuccess';
+import Center from '@/pages/Center';
 export default [
+  {
+    path: '/center',
+    component: Center,
+    meta: { show: true },
+  },
   {
     path: '/paysuccess',
     component: PaySuccess,
